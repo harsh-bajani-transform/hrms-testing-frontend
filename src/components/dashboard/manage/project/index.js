c1485af7cc56274f8fd74a394246ee405d66fe65
Naitik-5517 <naitik.maisuriya@transformsolution.net>
2026-03-02 16:37:31 +0530

    the repository has been empty

--- src/components/dashboard/manage/project/index.js
+++
@@ -1,6 +0,0 @@
-export { default } from './ProjectsManagement';
-export { default as ProjectCard } from './ProjectCard';
-export { default as AddProjectForm } from './AddProjectForm';
-export { default as AddProjectFormModal } from './AddProjectFormModal';
-export { default as ProjectHeader } from './ProjectHeader';
-export { default as TaskTable } from './TaskTable';
--- src/hooks/useDeviceInfo.js
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react';
-
-export function useDeviceInfo() {
-  // Use sessionStorage or fallback to defaults
-  const device_id = useMemo(() => sessionStorage.getItem('device_id') || 'adjisjd09734', []);
-  const device_type = useMemo(() => sessionStorage.getItem('device_type') || 'LAPTOP', []);
-  return { device_id, device_type };
-}
